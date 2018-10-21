3fa223ea2212bc6481f3283d715fa1390faecea5
lincolnsrjr <lincolnsrjr@gmail.com>
2018-10-21 16:56:20 -0300

    load files on input paht

---
+++ lib/splitter.rb
@@ -0,0 +1,18 @@
+class Splitter
+  attr_accessor :content
+
+  INPUT_PATH = 'data/input'.freeze
+  OUTPUT_PATH = 'data/output'.freeze
+
+  def load
+    Dir["../#{INPUT_PATH}/*"].each { |file_path| append_content(file_path) }
+    @content
+  end
+
+  private
+
+  def append_content(file_path)
+    @content ||= ''
+    @content << "#{File.open(file_path).read}\n"
+  end
+end
